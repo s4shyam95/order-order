b54663a00b7ed5fb306ee49d21773732e81cc1ce
Shyam Mehta <s4shyam95@gmail.com>
2020-07-11 14:26:56 -0400

    Admin enabled

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     url(r'^$',  views.chat_room, name='game'),
-    url(r'^conduct/$',  views.admin_room, name='admin'),
+    url(r'^conduct/$',  views.admin_room, name='conduct'),
+    url(r'^admin/', admin.site.urls, name='admin'),
 ]
