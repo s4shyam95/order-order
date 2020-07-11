87316ce2cbd4a3ab6e98255ae990981ce23e2e67
Shyam Mehta <s4shyam95@gmail.com>
2020-07-11 04:11:34 -0400

    Fix json accessing in python

--- chat/consumers.py
+++ chat/consumers.py
@@ -28,26 +28,26 @@ def ws_receive(message):
 
     if data:
         if data['type'] == 'join':
-            u = User.objects.get_or_create(name=data.handle)
+            u = User.objects.get_or_create(name=data['handle'])
             message.reply_channel.send({'text': json.dumps({'ping': 'pong'})})
 
         if data['type'] == 'answer':
-            author = User.objects.get(name=data.handle)
-            question = Question.objects.get(id=data.question)
+            author = User.objects.get(name=data['handle'])
+            question = Question.objects.get(id=data['question'])
             if len(question.answers.filter(by=author)) > 0:
                 message.reply_channel.send({'type': 'alert', 'message': 'Already Answered Question'})
             if question.hidden:
                 message.reply_channel.send({'type': 'alert', 'message': 'Question Not Open Yet'})
-            answer = Answer(by=author, for_q=question, ans=data.answer)
+            answer = Answer(by=author, for_q=question, ans=data['answer'])
             answer.save()
             message.reply_channel.send({'type': 'answer_response'})
 
         if data['type'] == 'unlock_question':
-            message.reply_channel.send({'type': 'unlock_question', 'question_id': data.question_id})
+            message.reply_channel.send({'type': 'unlock_question', 'question_id': data['question_id']})
 
         if data['type'] == 'show_answers':
             answers = None
-            question = Question.objects.get(id=data.question_id)
+            question = Question.objects.get(id=data['question_id'])
             answers = [{'player':answer.by.handle, 'answer':answer.ans, 'score':answer.score()} for answer in question.answers]
             message.reply_channel.send({'type': 'show_answer', 'answers': json.dumps(answers), 'correct_answer': question.correct_answer})
 
